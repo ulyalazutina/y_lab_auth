21eb1bfbac0b43c36f6fe42d3f045dd93e92e744
Ulyana Lazutina <ulyalazutina@yandex.ru>
2024-09-03 16:27:52 +0300

    Данные с формы

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { authUser } from './api/user';
 import styles from './App.module.css';
 import Button from './components/Button/Button';
 import Circle from './components/Circle/Circle';
@@ -6,12 +7,23 @@ import Title from './components/Title/Title';
 import Wrapper from './components/Wrapper/Wrapper';
 
 function App() {
+
+  const handleSubmitForm = (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.target);
+    authUser({
+      email: formData.get('email'),
+      password: formData.get('password'),
+    })
+  }
+
   return (
     <Wrapper>
       <Circle color={'pink'} />
       <div className={styles.box}>
       <Title>Аутентификация</Title>
-        <form action="#" className={styles.form}>
+        <form onSubmit={handleSubmitForm} action="#" className={styles.form}>
           <Input type={'email'} name={'email'} id={'email'} placeholder={'username@mail.ru'}>Почта</Input>
           <Input type={'password'} name={'password'} id={'password'} placeholder={'*******'}>Пароль</Input>
           <Button type={'submit'}>Войти</Button>
